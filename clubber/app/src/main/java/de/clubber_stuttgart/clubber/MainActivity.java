60d04450aca58772376d8aaaf64edc8a83e97464
Nico Burkart <nb094@hdm-stuttgart.de>
2019-03-28 22:36:34 +0100

    test

--- clubber/app/src/main/java/de/clubber_stuttgart/clubber/MainActivity.java
+++ clubber/app/src/main/java/de/clubber_stuttgart/clubber/MainActivity.java
@@ -1,9 +1,30 @@
 package de.clubber_stuttgart.clubber;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    BroadcastReceiver downloadReceiver;
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //registers an IntentFilter dynamically for DownloadReceiver. When a Download is finished, the DownloadReceiver.class will listen to it
+        IntentFilter downloadCompleteFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(downloadReceiver, downloadCompleteFilter);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        //unregisters the receiver
+        unregisterReceiver(downloadReceiver);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
