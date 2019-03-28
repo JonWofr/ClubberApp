dd782dcfdf83c8277b44402c1252a1ea12c5c8e9
Jonas Wolfram <jw132@hdm-stuttgart.de>
2019-03-28 17:52:35 +0100

    Created Android Studio project

---
+++ clubber/app/src/main/java/de/clubber_stuttgart/clubber/MainActivity.java
@@ -0,0 +1,13 @@
+package de.clubber_stuttgart.clubber;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
