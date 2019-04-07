95e3a6264df0854e21dbd0e6a46aec1ca6a7985a
Nico Burkart <nb094@hdm-stuttgart.de>
2019-04-07 13:38:37 +0200

    Implemented Service running in its own Thread which initiates the DBConnection

--- clubber/app/src/main/java/de/clubber_stuttgart/clubber/DBConnectionService.java
+++ clubber/app/src/main/java/de/clubber_stuttgart/clubber/DBConnectionService.java
@@ -1,7 +1,13 @@
 package de.clubber_stuttgart.clubber;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -12,14 +18,27 @@ import android.content.Intent;
  */
 public class DBConnectionService extends IntentService {
 
-    public DBConnectionService(String name) {
-        super(name);
+    public DBConnectionService() {
+        super("DBConnectionService");
     }
+
+    final private String LOG = "DBConnectionService";
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        //ToDo: Das hier in eine Methode schreiben, da wir das auch ausführen müssen, wenn der Nutzer wieder Internet hat oder einen Refresh durchführen will.
+        Log.i(LOG, "initiating setup and update of the database...");
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(this);
+        String[] highestIds = dataBaseHelper.selectHighestIds();
+        //init call to start async task which updates the database if needed.
+        Log.i(LOG, "Requesting data from webserver database...");
+        Log.d(LOG, "Requesting data from event table from following id:" + highestIds[0]);
+        Log.d(LOG, "Requesting data from club table from following id:" + highestIds[1]);
+        new HTTPHelper().initiateServerCommunication(highestIds[0], highestIds[1], this);
 
     }
 
-
 }
+
+
+
