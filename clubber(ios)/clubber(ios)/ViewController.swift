328a2c737955eca564a3e9b2ab4c885bf5db0f1f
Nico Burkart <nico@Nicos-MacBook-Pro.local>
2019-04-10 11:01:50 +0200

    init push rep for ios

---
+++ clubber(ios)/clubber(ios)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clubber(ios)
+//
+//  Created by Nico Burkart on 10.04.19.
+//  Copyright © 2019 hdm-stuttgart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
