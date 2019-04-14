2a906e5639b3f00b21d89f8cb03ca3872cb2f760
Nico Burkart <nb094@hdm-stuttgart.de>
2019-04-14 17:14:01 +0200

    (ios) connection to the json API on webserver + structure of DB Jonas and Nico

--- clubber(ios)/clubber(ios)/AppDelegate.swift
+++ clubber(ios)/clubber(ios)/AppDelegate.swift
@@ -12,9 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Everytime the application starts the database is beeing updated
+        HTTPHelper.requestResponseServer()
+        
         // Override point for customization after application launch.
         return true
     }
@@ -35,6 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
---
+++ clubber(ios)/clubber(ios)/DataBaseHelper.swift
@@ -0,0 +1,17 @@
+//
+//  DataBaseHelper.swift
+//  clubber(ios)
+//
+//  Created by Nico Burkart on 14.04.19.
+//  Copyright © 2019 hdm-stuttgart. All rights reserved.
+//
+
+import Foundation
+
+class DataBaseHelper : Thread{
+    
+    
+    
+    
+}
+
