6e111719a5ed285afc347d7c93f204e584a7e1fd
Nico Burkart <nb094@hdm-stuttgart.de>
2019-04-25 17:18:54 +0200

    (ios) server Request Json

--- clubber(ios)/clubber(ios)/HTTPHelper.swift
+++ clubber(ios)/clubber(ios)/HTTPHelper.swift
@@ -1,14 +1,21 @@
-//
 //  HTTPHelper.swift
 //  clubber(ios)
 //
 //  Created by Nico Burkart on 16.04.19.
 //  Copyright © 2019 hdm-stuttgart. All rights reserved.
-//
 
+
+import UIKit
 import Foundation
+import CoreData
+import os.log
 
-class HTTPHelper {
+class HTTPHelper{
+    
+    let log = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "network")
+
+    
+    //JSONData struct that stores Event and Club object Arrays
     struct JSONData : Decodable {
         var events :[Event]!
         var clubs : [Club]!
@@ -16,34 +23,155 @@ class HTTPHelper {
     
     struct Event : Decodable {
         var id : String!
-        var date : String!
+        var dte : String!
         var name : String!
+        var club : String!
+        var srttime : String!
+        var genre : String!
+        var btn : String!
     }
     
     struct Club : Decodable {
         var id : String!
         var name : String!
+        var adrs : String!
+        var tel : String!
+        var web : String!
     }
     
+    
     static func requestResponseServer(){
-        let url = "https://clubber-stuttgart.de/script/scriptDB.php"
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        
+        
+        func requestHighestId(entity: String) -> Int {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+            request.entity = NSEntityDescription.entity(forEntityName: entity, in: context)
+            request.resultType = NSFetchRequestResultType.dictionaryResultType
+            
+            let keypathExpression = NSExpression(forKeyPath: "id")
+            let maxExpression = NSExpression(forFunction: "max:", arguments: [keypathExpression])
+            
+            let key = "maxId"
+            
+            let expressionDescription = NSExpressionDescription()
+            expressionDescription.name = key
+            expressionDescription.expression = maxExpression
+            expressionDescription.expressionResultType = .integer64AttributeType
+            
+            request.propertiesToFetch = [expressionDescription]
+            
+            var maxId: Int? = 0
+            
+            
+            do{
+                if let result = try context.fetch(request) as? [[String: Int]], let dict = result.first {
+                    maxId = dict[key]
+                }
+                return maxId!
+            }
+            catch{
+                print(error)
+                return 0
+            }
+        }
+        
+        let highestEventId = requestHighestId(entity: "Events")
+        let highestClubId = requestHighestId(entity: "Clubs")
+        
+       
+        print(highestClubId)
+        
+        let url = "https://clubber-stuttgart.de/script/scriptDB.php?idEvent=\(highestEventId)&&idClub=\(highestClubId)"
         let urlObj = URL(string: url)
         
+        //starts Thread
         URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
             do {
+                //jsonData is object of JSONData struct which contains an Object Array of Event- and Club-struct
                 let jsonData = try JSONDecoder().decode(JSONData.self, from: data!)
                 
-                for event in jsonData.events {
-                    print(event.name)
-                }
-                for club in jsonData.clubs {
-                    print(club.name)
-                }
+                saveArraysInDatabase(jsonDataObj: jsonData)
+                
                 
             }catch{
-                print("ooooooppps!")
+                print(error)
             }
             
             }.resume()
+        
+        
+        func saveArraysInDatabase(jsonDataObj: JSONData) {
+            do{
+                for event in jsonDataObj.events {
+                    let eventMirror = Mirror(reflecting: event)
+                    let newEventEntry = NSEntityDescription.insertNewObject(forEntityName: "Events", into: context)
+                    for children in eventMirror.children{
+                        if (children.label! == "id"){
+                            let id = children.value as! String
+                            newEventEntry.setValue(Int(id), forKey: children.label!)
+                        }else{
+                            newEventEntry.setValue(children.value as? String ?? "N/A", forKey: children.label!)
+                        }
+                        print("value: \(children.value as! String) forKey: \(children.label!)")
+                        try context.save()
+                        print("saved")
+                    }
+                }
+                for club in jsonDataObj.clubs {
+                    let clubMirror = Mirror(reflecting: club)
+                    let newClubEntry = NSEntityDescription.insertNewObject(forEntityName: "Clubs", into: context)
+                    for children in clubMirror.children{
+                        if (children.label! == "id"){
+                            let id = children.value as! String
+                            newClubEntry.setValue(Int(id), forKey: children.label!)
+                        }else{
+                            newClubEntry.setValue(children.value as? String ?? "N/A", forKey: children.label!)
+                        }
+                        print("value: \(children.value as! String) forKey: \(children.label!)")
+                        try context.save()
+                        print("saved")
+                    }
+                }
+            }catch{
+                print("opps")
+                print(error)
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+/*
+ func requestDataFromDatabase(entity: String){
+ let request = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+ request.returnsObjectsAsFaults = false
+ do{
+ let results = try context.fetch(request)
+ if(results.count > 0){
+ 
+ for event in results {
+ print((event as AnyObject).value(forKey: "id") as! Int)
+ }
+ }
+ }
+ catch{
+ print(error)
+ }
+ }
+ */
+
+
+
+
+
+
+
--- clubber(ios)/clubber(ios)/ViewController.swift
+++ clubber(ios)/clubber(ios)/ViewController.swift
@@ -7,68 +7,14 @@
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
-        
-       
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        
-        let context = appDelegate.persistentContainer.viewContext
-        
-        //let newUser = NSEntityDescription.insertNewObject(forEntityName: "Events", into: context)
-        
-    
-        /*
-        newUser.setValue("nameTest", forKey: "name")
-        newUser.setValue("12.04.19", forKey: "date")
-        newUser.setValue(1, forKey: "id")
-        newUser.setValue("7grad", forKey: "club")
-        newUser.setValue("genreTest", forKey: "genre")
-        newUser.setValue("07:00", forKey: "srttime")
-        newUser.setValue("link", forKey: "btn")
-        
-        do{
-            
-            try context.save()
-            print("SAVED")
-            
-            
-            
-        }catch{
-            
-        }
-        
-    }
- */
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Events")
-        request.returnsObjectsAsFaults = false
-        
-        
-        do{
-            let results = try context.fetch(request)
-        
-            if(results.count > 0){
-                
-                for event in results {
-                    print((event as AnyObject).value(forKey: "date") as! String)
-                }
-            
-        }
-        
-        }catch{
-            
-        }
-        
         
 
 }
 
 }
+
